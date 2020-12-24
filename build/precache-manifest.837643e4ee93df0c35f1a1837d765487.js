6c16d3e8164728420f49554c21cabfbe6d7fa19b
snx <snx1029@163.com>
2020-12-24 22:08:29 +0800

    build problem

---
+++ build/precache-manifest.837643e4ee93df0c35f1a1837d765487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8a0f4d6a82c58105f92",
+    "url": "./static/css/main.a972245c.chunk.css"
+  },
+  {
+    "revision": "e8a0f4d6a82c58105f92",
+    "url": "./static/js/main.e8a0f4d6.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "2cf45a7618144b5197ba",
+    "url": "./static/css/2.48d5680e.chunk.css"
+  },
+  {
+    "revision": "2cf45a7618144b5197ba",
+    "url": "./static/js/2.2cf45a76.chunk.js"
+  },
+  {
+    "revision": "d032d16c78a2b7460ac4d86521721d93",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d0188320a69def7e8ed96a23c3408f7.js"
+  "./precache-manifest.837643e4ee93df0c35f1a1837d765487.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
